8e73f30d0d40262f64b74cd10fa7d6f9e74f0ac8
chrisck00 <102791021+chrisck00@users.noreply.github.com>
2025-10-28 12:46:48 +0900

    Update loradownload.py to handle multiple datasets
    Modified the script to download multiple datasets using a loop and updated the repository ID for the base model.

--- loradownload.py
+++ loradownload.py
@@ -1,8 +1,14 @@
 from huggingface_hub import snapshot_download
-dataset="piqa"
+dataset=["rte","bool_q","hellaswag","arc_challenge", "openbookqa","piqa","coqa","glue_qqp"]
+for data in dataset:
+    snapshot_download(
+        repo_id=f"Styxxxx/llama2_7b_lora-{data}",
+        local_dir=f"./lora/{data}",
+        local_dir_use_symlinks=False
+    )
 snapshot_download(
-    repo_id=f"Styxxxx/llama2_7b_lora-{dataset}",
-    local_dir=f"./lora/{dataset}",
+    repo_id=f"meta-llama/Llama-2-7b-hf",
+    local_dir=f"./base_model",
     local_dir_use_symlinks=False
 )
 #["rte","boolq","hellaswag","arc_challenge", "openbookqa","piqa","coqa"]
